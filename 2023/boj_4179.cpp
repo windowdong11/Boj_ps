fe9db6810c6297ce7f25a9bc7499914715a9bdb9
WonDongGyu <windowdong11@naver.com>
2023-06-19 04:55:12 +0000

    solved 4179

--- 2023/boj_4179.cpp
+++ 2023/boj_4179.cpp
@@ -17,7 +17,7 @@ int main() {
 	int h, w;
 	cin >> h >> w;
 	vector<string> board(h);
-	vector<vector<int>> v(h, vector<int>(w, 0));
+	vector<vector<int>> v(h, vector<int>(w, 1000 * 1000 * 1000));
 	priority_queue<pii> userq;
 	priority_queue<pii> fireq;
 	priority_queue<pii> tmpuserq;
@@ -25,16 +25,15 @@ int main() {
 	for (int y = 0; y < h; ++y)
 	{
 		cin >> board[y];
-		for (int x = 0; x < w; ++w)
+		for (int x = 0; x < w; ++x)
 		{
-			if (v[y][x] == 'J') {
+			if (board[y][x] == 'J') {
 				board[y][x] = '.';
 				tmpuserq.push({x, y});
-				v[y][x] = 1;
+				v[y][x] = 0;
 			}
-			else if (v[y][x] == 'F') {
+			else if (board[y][x] == 'F') {
 				tmpfireq.push({x, y});
-				v[y][x] = -1;
 			}
 		}
 	}
@@ -42,15 +41,18 @@ int main() {
 	int dy[] = {1, 0, -1, 0};
 	while (!tmpuserq.empty()) {
 		swap(userq, tmpuserq);
+		swap(fireq, tmpfireq);
 		while (!userq.empty()) {
 			int ncost, cx, cy;
 			tie(cx, cy) = userq.top();
 			userq.pop();
+			if (board[cy][cx] == 'F')
+				continue;
 			ncost = v[cy][cx] + 1;
-			for (int w = 0; w < 4; ++w) {
-				int nx = cx + dx[w], ny = cy + dy[w];
+			for (int d = 0; d < 4; ++d) {
+				int nx = cx + dx[d], ny = cy + dy[d];
 				if (OOB(nx, ny, w, h)) {
-					cout << ncost - 1;
+					cout << ncost;
 					return 0;
 				}
 				if (board[ny][nx] != '.' || v[ny][nx] <= ncost)
@@ -59,19 +61,19 @@ int main() {
 				tmpuserq.push({nx, ny});
 			}
 		}
-	}
-	while (!userq.empty()) {
-			int ncost, cx, cy;
-			tie(cx, cy) = userq.top();
-			userq.pop();
-			for (int w = 0; w < 4; ++w) {
-				int nx = cx + dx[w], ny = cy + dy[w];
+		while (!fireq.empty()) {
+			int cx, cy;
+			tie(cx, cy) = fireq.top();
+			fireq.pop();
+			for (int d = 0; d < 4; ++d) {
+				int nx = cx + dx[d], ny = cy + dy[d];
 				if (OOB(nx, ny, w, h) || board[ny][nx] != '.')
 					continue;
 				board[ny][nx] = 'F';
-				userq.push({nx, ny});
+				tmpfireq.push({nx, ny});
 			}
 		}
 	}
+	cout << "IMPOSSIBLE\n";
 	return 0;
 }
